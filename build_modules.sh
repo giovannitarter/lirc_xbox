f0716ff9299fefbc8faaa84d2cb1ab89b7070cb7
root <Giovanni Tarter>
2018-10-14 20:33:48 +0200

    build script

--- build_modules.sh
+++ build_modules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-HEADERS_DIR="/home/pupillo/uarchlinux/linux_kernel/linux-4.18.8"
+#HEADERS_DIR="/home/pupillo/uarchlinux/linux_kernel/linux-4.18.8"
 
+HEADERS_DIR="/lib/modules/4.15.0-36-generic/build/"
 export HEADERS
 
 pushd kernel_module
@@ -10,6 +11,12 @@ make clean
 make build HEADERS=$HEADERS_DIR
 RES="$?"
 popd
+
+if [ "$RES" != "0" ];
+then
+    echo "error compiling"
+    exit 1
+fi
 
 pushd xbox_remote_keymap
 make clean
@@ -23,6 +30,9 @@ then
     exit 1
 fi
 
+rm -rf output
+mkdir -p output
+find . -name "*.ko" -exec cp {} output \;
 
 
 
